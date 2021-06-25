7c573af3823f270da0a913afc9488bffec70d989
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-06-25 14:11:09 +0530

    update

---
+++ script.js
@@ -0,0 +1,50 @@
+const secondHand = document.querySelector(`.second-hand`);
+const minsHand = document.querySelector(`.min-hand`);
+const hourHand = document.querySelector(`.hour-hand`);
+
+
+function setDate() {
+     const now = new Date();
+
+     const seconds = now.getSeconds();
+    
+     const secondsDegrees  = ((seconds/60)* 360) + 90;
+    
+     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+
+     const mins = now.getMinutes();
+     const minsDegrees = ((mins / 60)*360) + 90;
+     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    
+
+     const hour = now.getHours();
+     const hoursDegrees = ( (hour / 12)*360) + 90;
+     minsHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    
+}
+
+
+setInterval(setDate ,1000);
+
+
+/*function setDate() {
+     const now = new Date();
+ 
+     const seconds = now.getSeconds();
+     const secondsDegrees = ((seconds / 60) * 360) + 90;
+     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+ 
+     const mins = now.getMinutes();
+     const minsDegrees = ((mins / 60) * 360) + 90;
+     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+ 
+     const hour = now.getMinutes();
+     const hoursDegrees = ((hour / 12) * 360) + 90;
+     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+     
+ }
+ 
+ setInterval(setDate, 1000);*/
